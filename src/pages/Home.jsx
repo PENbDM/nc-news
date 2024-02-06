419fd3cc6481a1638861a310cd4322b69a1bb82d
Dmytro <dimapen57@gmail.com>
2024-02-06 15:34:07 +0000

    7ticketVotesAndCommentsMoreGood

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,14 @@ function Home() {
   return (
     <div>
       <Header/>
-    Home
-    </div>
+      <div style={{ maxWidth: '1600px' }} className='mx-auto mt-10 ml-20 mr-20'>
+      <div className='flex mx-auto flex-col items-center justify-center '>
+        <p className='text-3xl'>Hi, this is the best news site, better than BBC</p>
+        <p className='text-xl'>In order to get acces to functionality of this web site you</p>
+        <p className='text-xl'>Have to be <span className='font-medium'>login</span></p>
+        </div>
+      </div>
+      </div>
   )
 }
 
