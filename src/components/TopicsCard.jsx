bc19f5c5202801d7e2afce912ca8946d3c7bdf4f
Dmytro <dimapen57@gmail.com>
2024-02-08 11:14:45 +0000

    12ErrorHandling

--- src/components/TopicsCard.jsx
+++ src/components/TopicsCard.jsx
@@ -4,7 +4,6 @@ export default function TopicsCard({topic,handleArticleByTopic}) {
   return (
     <div className='flex p-4 m-2 border border-solid border-green-500 flex-col items-center'>
         <p>Slug: <span className='font-semibold'>{topic.slug}</span></p>
-        <p>Description: <span className='font-semibold'>{topic.description}</span></p>
         <button className='mt-4 p-2 border border-solid border-green-500' onClick={()=>handleArticleByTopic(topic.slug)}>Get Articles by Topic</button>
     </div>
   )
